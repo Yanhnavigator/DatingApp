9bb5e6ab9378740f966f3c900352d21a91ad1690
Hai Yan <hai@Hai-MacBook.local>
2018-09-01 22:10:26 +1000

    initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        //type in ctor+ tap key to create a constructor 
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        //This is property represents a table in database
+        public DbSet<Value> Values { get; set; }
+    }
+}
